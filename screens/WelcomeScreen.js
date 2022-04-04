93b79002fb86fa7542308fa29a5b951c7489ca76
keyboardTC <37123270+keyboardTC@users.noreply.github.com>
2022-04-04 08:10:44 -0400

    Starter code

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+
+function WelcomeScreen() {
+
+  useEffect(() => {
+    // Make Api call
+  }, []);
+
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>You authenticated successfully!</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
